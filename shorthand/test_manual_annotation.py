eaa7a9c9676eb5816a874ffe05e3b4fc6d6412b4
devin <short.devin@gmail.com>
2022-06-23 15:10:13 -0700

    setup pytest workflow

---
+++ shorthand/test_manual_annotation.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import shorthand as shnd
+
+
+def test_wrk_synthesis():
+
+    s = shnd.Shorthand(
+        entry_syntax="shorthand/resources/default_entry_syntax.csv",
+        link_syntax="shorthand/resources/default_link_syntax.csv",
+        item_separator='__',
+        default_entry_prefix='wrk',
+        space_char='|',
+        na_string_values='!',
+        na_node_type='missing',
+        syntax_case_sensitive=False
+    )
+
+    parsed = s.parse_text(
+        'shorthand/test_data/manual_annotation.shnd',
+        skiprows=2,
+        comment_char='#'
+    )
+
+    synthesized = parsed.synthesize_entries('wrk', fill_spaces=True)
+
+    check = pd.Series([
+        'asmith_bwu__1999__s_bams__101__803__xxx',
+        'asmith_bwu__1998__s_bams__100__42__yyy',
+        'bjones__1975__s_jats__90__1__!',
+        'bwu__1989__t_long|title__!__80__!',
+        'Some|Author__1989__t_A|Title|With|\\#__!__!__!',
+        'asmith_bwu__2008__s_bams__110__1__zzz'
+    ])
+
+    assert (check == synthesized).all()
+
+
+def test_note_synthesis():
+
+    s = shnd.Shorthand(
+        entry_syntax="shorthand/resources/default_entry_syntax.csv",
+        link_syntax="shorthand/resources/default_link_syntax.csv",
+        item_separator='__',
+        default_entry_prefix='wrk',
+        space_char='|',
+        na_string_values='!',
+        na_node_type='missing',
+        syntax_case_sensitive=False
+    )
+
+    parsed = s.parse_text(
+        'shorthand/test_data/manual_annotation.shnd',
+        skiprows=2,
+        comment_char='#'
+    )
+
+    synthesized = parsed.synthesize_entries(entry_node_type='note')
+
+    check = pd.Series([
+        'this is an article I made up for testing',
+        'here\'s a note with an escaped\\__item separator and some "quotation marks"'
+    ])
+
+    assert (check == synthesized).all()
