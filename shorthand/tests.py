34df53ec2b469a1d40d283710627dfbe6d1152ce
devin <short.devin@gmail.com>
2022-06-24 14:41:00 -0700

    consolidate tests

--- shorthand/tests.py
+++ shorthand/tests.py
@@ -2,7 +2,7 @@ import pandas as pd
 import shorthand as shnd
 
 
-def test_wrk_synthesis():
+def test_manual_annotation_wrk_synthesis():
 
     s = shnd.Shorthand(
         entry_syntax="shorthand/resources/default_entry_syntax.csv",
@@ -35,7 +35,7 @@ def test_wrk_synthesis():
     assert (check == synthesized).all()
 
 
-def test_note_synthesis():
+def test_manual_annotation_note_synthesis():
 
     s = shnd.Shorthand(
         entry_syntax="shorthand/resources/default_entry_syntax.csv",
@@ -62,3 +62,38 @@ def test_note_synthesis():
     ])
 
     assert (check == synthesized).all()
+
+
+def test_single_column_wrk_synthesis():
+
+    s = shnd.Shorthand(
+        entry_syntax="shorthand/resources/default_entry_syntax.csv",
+        link_syntax="shorthand/resources/default_link_syntax.csv",
+        item_separator='__',
+        default_entry_prefix='wrk',
+        space_char='|',
+        na_string_values='!',
+        na_node_type='missing',
+        syntax_case_sensitive=False
+    )
+
+    parsed = s.parse_text(
+        'shorthand/test_data/single_column.shnd',
+        skiprows=0,
+        comment_char='#',
+        drop_na=False
+    )
+
+    synthesized = parsed.synthesize_entries('wrk', fill_spaces=True)
+
+    check = pd.Series([
+        'asmith_bwu__1999__s_bams__101__803__xxx',
+        'asmith_bwu__1998__s_bams__100__42__yyy',
+        'bjones__1975__s_jats__90__1__!',
+        'bwu__1989__t_long|title__!__80__!',
+        'Some|Author__1989__t_A|Title|With|\\#__!__!__!',
+        'asmith_bwu__2008__s_bams__110__1__zzz'
+    ])
+
+    assert (check == synthesized).all()
+
