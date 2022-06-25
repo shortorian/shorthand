c3af778c786f432b120c9da6b3cb89cced1780f4
devin <short.devin@gmail.com>
2022-06-24 22:29:04 -0700

    setup.py initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    description = readme.read()
+
+setuptools.setup(
+    name='shorthand',
+    version='0.1.0',
+    author='Devin Short',
+    author_email='short.devin@gmail.com',
+    packages=['shorthand'],
+    description=(
+        'A Python package for parsing text data according to custom '
+        'syntax files'
+    ),
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/shortorian/shorthand',
+    license='MIT',
+    python_requires='>=3.9',
+    install_requires=[]
+)
