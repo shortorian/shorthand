21c9d67ea2ac04a766e192b7fb0324403caf547c
devin <short.devin@gmail.com>
2022-07-05 14:08:57 -0700

    roll back parse_shorthand_file

--- shorthand/__init__.py
+++ shorthand/__init__.py
@@ -1,5 +1,4 @@
 from shorthand.Shorthand import Shorthand
-from shorthand.Shorthand import parse_shorthand_file
 from shorthand.ParsedShorthand import ParsedShorthand
 import shorthand.entry_parsing
 import shorthand.syntax_parsing
