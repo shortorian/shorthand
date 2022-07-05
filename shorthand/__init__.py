d95abcdf66d87a2d4be754032ed983ae45a4cbc0
devin <short.devin@gmail.com>
2022-07-05 13:56:58 -0700

    import parse_shorthand_file

--- shorthand/__init__.py
+++ shorthand/__init__.py
@@ -1,4 +1,5 @@
 from shorthand.Shorthand import Shorthand
+from shorthand.Shorthand import parse_shorthand_file
 from shorthand.ParsedShorthand import ParsedShorthand
 import shorthand.entry_parsing
 import shorthand.syntax_parsing
