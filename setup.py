b5e12ebe03a57d6c19dcb64ae7a5f024128b95e6
devin <short.devin@gmail.com>
2022-07-19 10:07:16 -0700

    release 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='shorthand',
-    version='0.1.0',
+    version='0.1.1',
     author='Devin Short',
     author_email='short.devin@gmail.com',
     packages=['shorthand'],
---
+++ shorthand-0.1.1/shorthand/__init__.py
@@ -0,0 +1,5 @@
+from shorthand.Shorthand import Shorthand
+from shorthand.ParsedShorthand import ParsedShorthand
+import shorthand.entry_parsing
+import shorthand.syntax_parsing
+import shorthand.util
